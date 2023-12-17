f5fb177c2ee1411d95314eb98e86c14648501e96
Dantitan1976 <dantitan1976@gmail.com>
2023-12-17 15:17:45 +0100

    exam_02

--- round_1/ft_swap/ft_swap.c
+++ round_1/ft_swap/ft_swap.c
@@ -6,7 +6,7 @@
 /*   By: dramirez </var/mail/dramirez>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 09:03:12 by dramirez          #+#    #+#             */
-/*   Updated: 2023/08/22 20:13:20 by dramirez         ###   ########.fr       */
+/*   Updated: 2023/12/17 15:16:55 by dramirez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	ft_swap(int *a, int *b)
 	*b = prov;
 }
 
-int	main(int argc, char **argv)
+/*int	main(int argc, char **argv)
 {
 	int	num_a;
 	int	num_b;
@@ -40,4 +40,4 @@ int	main(int argc, char **argv)
 		printf("El valor de a es ahora %d y el de b es %d\n", *a, *b);
 	}
 	return (0);
-}
+}*/
